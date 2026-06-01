c8ec104677c0492b46f48cffa44e890d91acdca6
nithinnithin296007-code <nithinnithin296007@gmail.com>
2026-06-01 21:46:29 +0530

    fix cors env variable

--- server/app.js
+++ server/app.js
@@ -7,7 +7,10 @@ const tourRoutes = require('./routes/tours');
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: ['http://localhost:3000', 'https://sracholidays.vercel.app'], credentials: true }));
+app.use(cors({ 
+  origin: process.env.CLIENT_URL || 'http://localhost:3000', 
+  credentials: true 
+}));
 app.use(express.json());
 
 const limiter = rateLimit({
